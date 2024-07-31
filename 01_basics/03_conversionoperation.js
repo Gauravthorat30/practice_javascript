30b36ce4727b7443c00b6749e34151779e258b51
Gauravthorat30 <162902777+Gauravthorat30@users.noreply.github.com>
2024-07-31 18:33:29 +0000

    conversionofdatatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,23 @@
+let string = "20";
+console.log(typeof string);
+console.log(string);
+
+let stringNumber = Number(string);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+let boolean = true;
+console.log(typeof boolean);
+console.log(boolean);
+
+let booleanNumber = Number(boolean);
+console.log(typeof booleanNumber);
+console.log(booleanNumber)
+
+let number = 20;
+console.log(typeof number);
+console.log(number);
+
+let numberString = String(number);
+console.log(typeof numberString);
+console.log(numberString);
