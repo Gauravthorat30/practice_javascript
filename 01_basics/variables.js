f13eabf1a3fb322943e2ab98422c652d4fe4455a
Gauravthorat30 <162902777+Gauravthorat30@users.noreply.github.com>
2024-07-31 14:01:34 +0000

    Renamed test.js to 01_basics/test.js and added variables.js

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345;
+let accountEmail = "gaurav@gmail.com";
+var accountPassword = "0000";
+accountCity = "mumbai";
+let accountState; 
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState]);
