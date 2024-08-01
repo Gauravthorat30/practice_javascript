9b01f1f5daf1271e71970ac48df6b5358ca3fac7
Gauravthorat30 <162902777+Gauravthorat30@users.noreply.github.com>
2024-08-01 12:17:22 +0000

    datatypes summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,30 @@
+const number = 21;
+const string = "gaurav";
+const boolean = true;
+const bigint = 12345678901234567890n;
+const nulll = null;
+const id = Symbol('123');
+const anotherid = Symbol('12345678');
+const undefinedd = undefined;
+
+const arr = [1 , 2 , 3];
+const obj = {
+    name : "gaurav",
+    age : 21
+}
+const myFunction = function(){
+    console.log("function");
+}
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof bigint);
+console.log(typeof nulll);
+console.log(typeof undefinedd);
+console.log(id);
+console.log(anotherid);
+console.log(typeof arr);
+console.log(typeof obj);
+console.log(typeof myFunction);
+
