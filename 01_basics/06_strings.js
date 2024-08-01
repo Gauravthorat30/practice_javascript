e1ba3445acd4b95c07d7916b1bd2206cef3e6300
Gauravthorat30 <162902777+Gauravthorat30@users.noreply.github.com>
2024-08-01 15:33:49 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,7 @@
+let myName = new String("Hii my name is Gaurav");
+let index = 8;
+
+console.log(`The word at index ${index} is ${myName.at(index)}`);
+
+console.log(`The character code of ${myName.charAt(index)} is ${myName.charCodeAt(index)}`);
+
