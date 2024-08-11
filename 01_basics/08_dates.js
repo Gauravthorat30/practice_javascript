75359f424d0f6d0f79adbd1b1c0f159989f41499
Gauravthorat30 <162902777+Gauravthorat30@users.noreply.github.com>
2024-08-11 17:12:40 +0000

    dates

--- 01_basics/08_dates.js
+++ 01_basics/08_dates.js
@@ -1,2 +1,10 @@
 let mydate = new Date();
-console.log(mydate.getTime())
+console.log(mydate);
+console.log(mydate.getTime());
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+
+let myCreatedDate = new Date(2024 , 10 , 30);
+console.log(myCreatedDate.getTime());
+
